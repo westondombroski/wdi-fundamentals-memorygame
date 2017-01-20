da38bebf08ebc33ef570ed69c0b9259b98a3ae0d
westondombroski <westondombroski@gmail.com>
2017-01-19 20:25:33 -0800

    created game logic alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,18 @@
+/* Section 9, I think I forgot to comment for Section 8's Assignment*/
+
 console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if(cardOne === cardTwo || cardThree === cardFour) {
+  alert("You found a match!");
+}
+else if(cardOne !== cardFour || cardOne !== cardThree) {
+  alert("Sorry, try again");
+}
+else if(cardTwo !== cardThree || cardTwo !== cardFour) {
+  alert("Sorry, try again");
+};
