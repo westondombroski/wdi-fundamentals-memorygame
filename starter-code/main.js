0243697971e2f0e0df481d15f8717ccda849df05
westondombroski <westondombroski@gmail.com>
2017-01-21 23:08:11 -0800

    organizing code and creating dynamic html

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,18 +1,28 @@
-/* Section 9, I think I forgot to comment for Section 8's Assignment*/
+/* Section 10*/
 
-console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if(cardOne === cardTwo || cardThree === cardFour) {
   alert("You found a match!");
 }
-else if(cardOne !== cardFour || cardOne !== cardThree) {
-  alert("Sorry, try again");
-}
-else if(cardTwo !== cardThree || cardTwo !== cardFour) {
+else {
   alert("Sorry, try again");
 };
+*/
+
+var board = document.getElementById('game-board');
+
+var createCards = function (){
+  for(var i = 0; i < 4; i+=1) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    board.appendChild(newCard);
+  }
+}
+
+createCards();
